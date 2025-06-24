32cff5abd584ed6233c6f79baf74e3991e245f51
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-06-24 11:48:47 +0700

    feat: achievements pages

---
+++ src/pages/achievements/academic/index.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchAchievements, setKeyword, setPage, setActivityType, setAchievementType, setCompetitionLevel } from '../../../redux/achievements/actions'
+import { activityTypeOptions, achievementTypeOptions, competitionLevelOptions } from '../../../constants'
+import Breadcrumb from '../../../components/Breadcrumb'
+import SearchInput from '../../../components/SearchInput'
+import Dropdown from '../../../components/Dropdown'
+import Table from '../../../components/Table'
+
+const AcademicAchievementsPage = () => {
+  const dispatch = useDispatch()
+
+  const achievements = useSelector((state) => state.achievements)
+
+  useEffect(() => {
+    dispatch(fetchAchievements(undefined, 'Akademik'))
+  }, [
+    dispatch,
+    achievements.keyword,
+    achievements.page,
+    achievements.activity_type,
+    achievements.achievement_type,
+    achievements.competition_level,
+  ])
+
+  return (
+    <>
+      <Breadcrumb secondLevelText="Prestasi Akademik" />
+      <h1 className="fs-3">Prestasi Akademik</h1>
+      <div className="mb-3">
+        <div className="row g-2 align-items-end">
+          <div className="col-12 col-md-4">
+            <SearchInput
+              query={achievements.keyword}
+              handleChange={(e) => {
+                dispatch(setKeyword(e.target.value))
+                dispatch(setPage(1))
+              }}
+            />
+          </div>
+          <div className="col-12 col-md-2">
+            <Dropdown
+              name="activity_type"
+              value={achievements.activity_type}
+              onChange={e => {
+                dispatch(setActivityType(e.target.value))
+                dispatch(setPage(1))
+              }}
+              options={activityTypeOptions}
+            />
+          </div>
+          <div className="col-12 col-md-3">
+            <Dropdown
+              name="achievement_type"
+              value={achievements.achievement_type}
+              onChange={e => {
+                dispatch(setAchievementType(e.target.value))
+                dispatch(setPage(1))
+              }}
+              options={achievementTypeOptions}
+            />
+          </div>
+          <div className="col-12 col-md-3">
+            <Dropdown
+              name="competition_level"
+              value={achievements.competition_level}
+              onChange={e => {
+                dispatch(setCompetitionLevel(e.target.value))
+                dispatch(setPage(1))
+              }}
+              options={competitionLevelOptions}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="table-responsive">
+        <Table
+          status={achievements.status}
+          thead={[
+            'Nama Prestasi',
+            'Tanggal',
+            'Jenis Kegiatan',
+            'Jenis Prestasi',
+            'Tingkat Kompetisi',
+            'Nama Mahasiswa',
+          ]}
+          data={achievements.data}
+          tbody={[
+            'name',
+            'date',
+            'activity_type',
+            'achievement_type',
+            'competition_level',
+            'student_name',
+          ]}
+          pages={achievements.pages}
+          withPagination
+          handlePageClick={({ selected }) => dispatch(setPage(selected + 1))}
+        />
+      </div>
+    </>
+  )
+} 
+
+export default AcademicAchievementsPage
